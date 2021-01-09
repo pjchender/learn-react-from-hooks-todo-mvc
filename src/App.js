aa1a0d3c6cb1e1f2864bc32d01b08b80e9a06bc1
pjchender <pjchender@gmail.com>
2021-01-09 22:18:30 +0800

    Create SVG Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.scss';
 
+import { ReactComponent as CheckActive } from './assets/images/check-active.svg';
+import { ReactComponent as CheckCircle } from './assets/images/check-circle.svg';
+import { ReactComponent as CheckHover } from './assets/images/check-hover.svg';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -22,18 +25,7 @@ function App() {
         <div className="task-item">
           <div>
             <span className="d-inline-flex align-items-center justify-content-center icon">
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 20c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8m0-17c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9"
-                ></path>
-              </svg>
+              <CheckCircle />
             </span>
           </div>
           <div className="task-item-body">
@@ -46,18 +38,7 @@ function App() {
         <div className="task-item edit">
           <div>
             <span className="d-inline-flex align-items-center justify-content-center icon">
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <g fill-rule="evenodd">
-                  <path d="M12 20c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8m0-17c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9"></path>
-                  <path d="M10.9902 13.3027l-2.487-2.51-.71.704 3.193 3.224 5.221-5.221-.707-.707z"></path>
-                </g>
-              </svg>
+              <CheckHover />
             </span>
           </div>
           <div className="task-item-body">
@@ -75,18 +56,7 @@ function App() {
         <div className="task-item destroyed">
           <div>
             <span className="d-inline-flex align-items-center justify-content-center icon">
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.9854 15.0752l-3.546-3.58 1.066-1.056 2.486 2.509 4.509-4.509 1.06 1.061-5.575 5.575zm1.015-12.075c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z"
-                ></path>
-              </svg>
+              <CheckActive />
             </span>
           </div>
           <div className="task-item-body">
