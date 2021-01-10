a3649aa05f199477a9f8bd7d70d73d43b20c29a9
pjchender <pjchender@gmail.com>
2021-01-10 15:54:06 +0800

    Add Todos component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Todos from './components/Todos';
 import clsx from 'clsx';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
@@ -113,37 +114,11 @@ function App() {
         </div>
       </div>
 
-      <div className="todos">
-        {todos.map((todo) => (
-          <div
-            className={clsx('task-item', { done: todo.isDone })}
-            key={todo.id}
-          >
-            <div className="task-item-checked">
-              <span
-                className="icon icon-checked"
-                onClick={handleToggleIsDone(todo.id)}
-              />
-            </div>
-            <div className="task-item-body">
-              <span className="task-item-body-text">{todo.text}</span>
-              <input
-                className="task-item-body-input"
-                type="text"
-                placeholder="新增工作"
-              />
-            </div>
-            <div className="task-item-action">
-              <button
-                className="btn-reset btn-destroy icon"
-                onClick={handleDelete(todo.id)}
-              >
-                {' '}
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Todos
+        todos={todos}
+        handleDelete={handleDelete}
+        handleToggleIsDone={handleToggleIsDone}
+      />
 
       <Footer numOfTodos={numOfTodos} />
     </div>
