36f50bebf251054869cf702a2e84c74f04a3b0c2
pjchender <pjchender@gmail.com>
2021-01-24 01:11:25 +0800

    [fbLogin] API integration

--- src/App.js
+++ src/App.js
@@ -137,7 +137,125 @@ function App() {
     fetchTodos();
   }, []);
 
-  console.log('[render] app');
+  const [isFBInitialized, setIsFBInitialized] = useState(false);
+  const [authResponse, setAuthResponse] = useState();
+
+  useEffect(() => {
+    // SDK 載入完成時會立即呼叫 fbAsyncInit
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: process.env.REACT_APP_FB_APP_ID,
+        cookie: true,
+        xfbml: true,
+        version: process.env.REACT_APP_FB_APP_VERSION,
+      });
+
+      console.log('[fbAsyncInit] after window.FB.init');
+      setIsFBInitialized(true);
+      window.FB.AppEvents.logPageView();
+    };
+
+    // load facebook sdk script
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }, []);
+
+  // 取得使用者登入狀態
+  useEffect(() => {
+    if (!isFBInitialized) {
+      return;
+    }
+    window.FB.getLoginStatus(function (response) {
+      console.log('[getLoginStatus]', response);
+      setAuthResponse(response);
+    });
+  }, [isFBInitialized]);
+
+  // 呼叫 Graph API 取得使用者資料
+  useEffect(() => {
+    if (!isFBInitialized) {
+      return;
+    }
+
+    if (!authResponse) {
+      return;
+    }
+
+    if (authResponse?.status !== 'connected') {
+      return;
+    }
+
+    window.FB.api('/me', 'GET', { fields: 'name,email' }, function (response) {
+      console.log('[/me]', response);
+    });
+  }, [isFBInitialized, authResponse]);
+
+  useEffect(() => {
+    // FB 需已經初始化
+    if (!isFBInitialized) {
+      return;
+    }
+
+    // 已經確認使用者登入狀態
+    if (!authResponse) {
+      return;
+    }
+
+    if (authResponse?.status !== 'connected') {
+      console.log('使用者未登入，導回登入頁');
+      return;
+    }
+
+    console.log('合法的使用者');
+  }, [isFBInitialized, authResponse]);
+
+  // 使用者點擊登入
+  const handleFBLogin = () => {
+    if (!isFBInitialized) {
+      return;
+    }
+
+    window.FB.login(
+      function (response) {
+        console.log('handleFBLogin', response);
+        setAuthResponse(response);
+      },
+      { scope: 'public_profile,email' }
+    );
+  };
+
+  // 使用者登出
+  const handleFBLogout = () => {
+    // FB SDK 已經初始化
+    if (!isFBInitialized) {
+      return;
+    }
+
+    // 已經確認使用者登入狀態
+    if (!authResponse) {
+      return;
+    }
+
+    // 使用者尚未登入
+    if (authResponse?.status !== 'connected') {
+      return;
+    }
+
+    window.FB.logout(function (response) {
+      console.log('handleFBLogout', response);
+      setAuthResponse(response);
+    });
+  };
+
   return (
     <div className="app">
       <Header />
@@ -156,6 +274,9 @@ function App() {
         handleToggleIsDone={handleToggleIsDone}
         updateIsEdit={updateIsEdit}
       />
+
+      <button onClick={handleFBLogin}>Facebook Login</button>
+      <button onClick={handleFBLogout}>Facebook Logout</button>
 
       <Footer numOfTodos={numOfTodos} />
     </div>
